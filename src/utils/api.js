399df4502762c3461e8213367d6dbd247cb92652
FlorenciaRar <florenciastrugo@gmail.com>
2024-06-18 17:20:07 -0300

    commit for pull dev

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,5 @@
 import axios from "axios";
-
-export const getAccommodations = (dataList) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (dataList.length) {
-        resolve(dataList);
-      } else {
-        reject("Error");
-      }
-    }, 2000);
-  });
-
-export const getAccommodationDetail = (data, id) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (data.length) {
-        resolve(data.find((accommodation) => accommodation.idAlojamiento === parseInt(id)));
-      } else {
-        reject("Error");
-      }
-    }, 2000);
-  });
+import { geocode } from "react-geocode";
 
 export const getData = async (url) => {
   try {
@@ -40,27 +19,22 @@ export const deleteData = async (url) => {
   }
 };
 
-/* El setTimeout lo pongo aca solamente como para
-    simular un tiempo de respuesta de cuando se pide la data
-    y que tenga un pequelo rendering condicional con un ternario en el
-    componente "container" para que mientras se procesa la peticion
-    el usuario vea un spinner o un mensaje de carga. */
-
 // Función para obtener la ciudad basada en las coordenadas geográficas
 //En el tp la estructura que nos tiran a usar tiene latitud y longitud, por eso
 //busque info de como usar una api que convierta esos valores a la ciudad
-export function obtenerCiudad(latitud, longitud) {
-  const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitud}&lon=${longitud}&zoom=18&addressdetails=1`;
 
-  return fetch(apiUrl)
+export function obtenerCiudad(latitud, longitud) {
+  return geocode("latlng", `${latitud},${longitud}`, {
+    key: "AIzaSyDZmqbRMOVEJcGQj7g9Ssin-wWcYPMGoxM",
+  })
     .then((response) => {
-      if (!response.ok) {
+      if (!response) {
         throw new Error("Error al obtener la respuesta de la API");
       }
-      return response.json();
+      return response;
     })
     .then((data) => {
-      const city = data.address.city || data.address.suburb || data.address.town;
+      const city = data.results[0].address_components[3].long_name;
       return city;
     })
     .catch((error) => {
