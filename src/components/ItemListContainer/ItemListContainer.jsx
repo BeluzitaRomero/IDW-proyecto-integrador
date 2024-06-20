e4bbe680de41b1b6901d06d3e2a7c6ca448cd934
Carla <macarlabr@gmail.com>
2024-06-20 00:44:56 -0300

    Botones

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import './ItemListContainer.css'; // Importar los estilos CSS
+import "./ItemListContainer.css"; // Importar los estilos CSS
 import ItemList from "../ItemList/ItemList";
 import { getData } from "../../utils/api";
 import Loading from "../Loading/Loading";
@@ -11,8 +11,7 @@ const ItemListContainer = () => {
   const [countBanios, setCountBanios] = useState(0);
   const [filter, setFilter] = useState({});
   const [showPricePopup, setShowPricePopup] = useState(false);
-  const [showDormitoriosOptions, setShowDormitoriosOptions] = useState(false); // Estado para controlar la visibilidad del selector de dormitorios
-  const [showBaniosOptions, setShowBaniosOptions] = useState(false); // Estado para controlar la visibilidad del selector de baños
+
 
   const fetchAlojamientos = "http://localhost:3001/alojamiento/getAlojamientos";
   const fetchTipos = "http://localhost:3001/tiposAlojamiento/getTiposAlojamiento/";
@@ -26,30 +25,18 @@ const ItemListContainer = () => {
     setShowPricePopup(!showPricePopup);
   };
 
-  const toggleDormitoriosOptions = () => {
-    setShowDormitoriosOptions(!showDormitoriosOptions);
-  };
 
-  const toggleBaniosOptions = () => {
-    setShowBaniosOptions(!showBaniosOptions);
-  };
-
-  const handleDormitoriosSelect = (value) => {
-    setCountDormitorios(parseInt(value));
-    setShowDormitoriosOptions(false); // Ocultar el selector de dormitorios después de seleccionar
-  };
-
-  const handleBaniosSelect = (value) => {
-    setCountBanios(parseInt(value));
-    setShowBaniosOptions(false); // Ocultar el selector de baños después de seleccionar
-  };
 
   const filteredData = accommodations.filter((accommodation) => {
     return (
-      (!filter.rangoPrecio || parseInt(filter.rangoPrecio) === 0 || accommodation.PrecioPorDia <= parseInt(filter.rangoPrecio)) &&
+      (!filter.rangoPrecio ||
+        parseInt(filter.rangoPrecio) === 0 ||
+        accommodation.PrecioPorDia <= parseInt(filter.rangoPrecio)) &&
       (!filter.Estado || accommodation.Estado === filter.Estado) &&
-      (!filter.tipoAlojamiento || accommodation.idTipoAlojamiento === parseInt(filter.tipoAlojamiento)) &&
-      (!filter.Dormitorios || accommodation.CantidadDormitorios <= parseInt(filter.Dormitorios)) &&
+      (!filter.tipoAlojamiento ||
+        accommodation.idTipoAlojamiento === parseInt(filter.tipoAlojamiento)) &&
+      (!filter.Dormitorios ||
+        accommodation.CantidadDormitorios <= parseInt(filter.Dormitorios)) &&
       (!filter.Banios || accommodation.CantidadBanios <= parseInt(filter.Banios))
     );
   });
@@ -72,101 +59,146 @@ const ItemListContainer = () => {
 
   return (
     <>
-      <div className="flex-space2 crud-inputs">
-        <select name="tipoAlojamiento" id="" onChange={handleFilter} className="crud-input">
-          <option value="">Tipo</option>
-          {accommodationsType &&
-            accommodationsType.map((type) => (
-              <option key={type.idTipoAlojamiento} value={type.idTipoAlojamiento}>
-                {type.Descripcion}
-              </option>
-            ))}
-        </select>
-        <select name="Estado" id="" onChange={handleFilter} className="crud-input">
-          <option value="">Disponibilidad</option>
-          <option value="Disponible">Disponible</option>
-          <option value="Reservado">Reservado</option>
-        </select>
-
-        {/* Botón "Precio" que muestra/oculta la ventana emergente */}
-        <div className="price-container">
-          <button className="crud-input" onClick={togglePricePopup}>
-            Precio
-          </button>
-          {showPricePopup && (
-            <div className="price-popup">
-              <input
-                type="range"
-                name="rangoPrecio"
-                min="0"
-                max="13000"
-                step="1"
-                value={filter.rangoPrecio || 0}
-                onChange={handleFilter}
-              />
-              <p>Precio seleccionado: {filter.rangoPrecio || 0}</p>
-            </div>
-          )}
-        </div>
-
-        {/* Selector de dormitorios */}
-        <div className="price-container">
-          <button className="crud-input" onClick={toggleDormitoriosOptions}>
-            Dormitorios
-          </button>
-          {showDormitoriosOptions && (
-            <div className="price-popup square-options">
-              <p>Seleccione cantidad de dormitorios:</p>
-              <div className="options-container">
-                {[1, 2, 3, 4, 5, 6].map((option) => (
-                  <div
-                    key={option}
-                    className={`option ${countDormitorios === option ? 'selected' : ''}`}
-                    onClick={() => handleDormitoriosSelect(option)}
-                  >
-                    {option}
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-
-        {/* Selector de baños */}
-        <div className="price-container">
-          <button className="crud-input" onClick={toggleBaniosOptions}>
-            Baños
-          </button>
-          {showBaniosOptions && (
-            <div className="price-popup square-options">
-              <p>Seleccione cantidad de baños:</p>
-              <div className="options-container">
-                {[1, 2, 3, 4, 5, 6].map((option) => (
-                  <div
-                    key={option}
-                    className={`option ${countBanios === option ? 'selected' : ''}`}
-                    onClick={() => handleBaniosSelect(option)}
-                  >
-                    {option}
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-
-      </div>
-
-      <section id="explorar" className="content-section m-y">
-        <h2 className="section-title">Estas en IDW. Estas en casa</h2>
-        <hr />
+      <section id="explorar" className="content-section titulo">
+        <h2 className="section-title">Encontrá tu Lugar</h2>
         <p>
-          Ya sea que estés planeando unas vacaciones relajantes en la playa, una escapada romántica en la montaña o un viaje lleno de aventuras en la
+          Ya sea que estés planeando unas vacaciones relajantes en la playa, una
+          escapada romántica en la montaña o un viaje lleno de aventuras en la
           ciudad, estamos aquí para hacer que tu experiencia sea inolvidable.
         </p>
+        <hr />
 
+        {/* Mostrar lista de alojamientos o loading */}
         {filteredData ? <ItemList list={filteredData} /> : <Loading />}
       </section>
+
+      <div className="contenedor m-y">
+        <div className="flex-space crud-inputs">
+          <div className="input-container">
+            <select
+              name="tipoAlojamiento"
+              id=""
+              onChange={handleFilter}
+              className="crud-input"
+            >
+              <option value="">Tipo de Propiedad</option>
+              {accommodationsType &&
+                accommodationsType.map((type) => (
+                  <option key={type.idTipoAlojamiento} value={type.idTipoAlojamiento}>
+                    {type.Descripcion}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div className="input-container">
+            <select name="Estado" id="" onChange={handleFilter} className="crud-input">
+              <option value="">Disponibilidad</option>
+              <option value="Disponible">Disponible</option>
+              <option value="Reservado">Reservado</option>
+            </select>
+          </div>
+
+          <div className="input-container">
+            {/* Botón "Precio" que muestra/oculta la ventana emergente */}
+            <div className="price-container">
+              <div className="precio">
+                <button className="crud-input" onClick={togglePricePopup}>
+                  <label>
+                    Precio
+                    <input
+                      type="range"
+                      name="rangoPrecio"
+                      min="0"
+                      max="500000"
+                      step="1"
+                      onChange={handleFilter}
+                    />
+                    <span> ${filter.rangoPrecio || 0}</span>
+                  </label>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="input-container dormitorio">
+            {/* Selector de Dormitorios */}
+            <div className="price-container">
+              <div className="precio crud-input">
+                <label>
+                  <span className="label-dormitorio">Dormitorios</span>
+                  <div className="count-control">
+                    <button
+                      className="count-button"
+                      onClick={() => setCountDormitorios((prev) => Math.max(prev - 1, 0))}
+                    >
+                      <p className="signo">-</p>
+                    </button>
+                    <input
+                      type="text"
+                      className="count-input"
+                      name="Dormitorios"
+                      value={countDormitorios}
+                      onChange={handleFilter}
+                      onKeyDown={(e) => {
+                        // Permite solo números
+                        if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+                          e.preventDefault(); // Evita la acción predeterminada de las flechas de arriba/abajo
+                        }
+                      }}
+                    />
+                    <button
+                      className="count-button"
+                      onClick={() => setCountDormitorios((prev) => prev + 1)}
+                    >
+                      <p className="signo">+</p>
+                    </button>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="input-container dormitorio">
+            {/* Selector de Baños */}
+            <div className="price-container">
+              <div className="precio crud-input">
+                <label>
+                  <span className="label-dormitorio">Baños</span>
+                  <div className="count-control">
+                    <button
+                      className="count-button"
+                      onClick={() => setCountBanios((prev) => Math.max(prev - 1, 0))}
+                    >
+                      <p className="signo">-</p>
+                    </button>
+                    <input
+                      type="text"
+                      className="count-input"
+                      name="Banios"
+                      value={countBanios}
+                      onChange={handleFilter}
+                      onKeyDown={(e) => {
+                        // Permite solo números
+                        if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+                          e.preventDefault(); // Evita la acción predeterminada de las flechas de arriba/abajo
+                        }
+                      }}
+                    />
+                    <button
+                      className="count-button"
+                      onClick={() => setCountBanios((prev) => prev + 1)}
+                    >
+                      <p className="signo">+</p>
+                    </button>
+                  </div>
+                </label>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </>
   );
 };
